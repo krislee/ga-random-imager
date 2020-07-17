7b6d3e77cbb31f69a5753e3b770b70775e22d6d3
Kristy Lee <leekristy01@gmail.com>
2020-07-17 19:11:02 -0400

    moved code from subfolder to parent folder, random-imager and deleted subfolder

--- random_imager/app.js
+++ random_imager/app.js
@@ -17,28 +17,49 @@ const cards = [
     'https://www.trustedtarot.com/img/cards/the-moon.png'
   ]
 
+//Make black div 
 const $addDiv = $('<div>').addClass('addCard')
 const $para = $('<p>').text('Add a card').addClass('para')
 $addDiv.append($para)
 $('.flex').append($addDiv)
 
-const renderCard = () => {
-    const randomNumber = Math.floor(Math.random()*cards.length)
-    const $newDiv = $('<div>').addClass('newDivs')
-    $newDiv.css('background-image', `url(${cards[randomNumber]})`)
-    $('.flex').append($newDiv)
-    $newDiv.on('click', (e) => {
-        $newDiv.remove()
-        //or do e.target.remove()
-    })
-    // $newDiv.on('click', (e) => {
-    //     $newDiv.css('display', 'none')
-    //     console.log(e.target)
-    //     console.log($newDiv)
-    // })
-}
-$addDiv.on('click', renderCard)
+//Add new image to array
+$('button').on('click', () => {
+  const $input = $('[name="imgURL"]')
+  if($input.val()){
+    cards.push($input.val())
+    $input.val('')
+  } else {
+    alert('Input an image URL')
+  }
+})
 
+//Add image to div.flex but will remove the image from the array after it is done so it doesn't
+//get reused later & added an alt attribute
+const tarot = () => {
+  if(cards.length>0){
+    const randomNum = Math.floor(Math.random()*cards.length)
+    // const alt = cards[randomNum].slice(39,-4)
+    // const $img = $('<img>').attr('src', `${cards[randomNum]}`).attr('alt',`${alt}`)
+    const arrayAlt = cards[randomNum].split('/')
+    const alt = arrayAlt[arrayAlt.length-1].slice(0,-4)
+    let newAlt
+    if(alt.includes('-')) {
+      newAlt = alt.replace(/-/gi," ")
+    } else {
+      newAlt = alt
+    }
+    const $img = $('<img>').attr('src', cards[randomNum]).attr('alt',`${newAlt}`)
+    $('.flex').append($img)
+    $img.on('click', () => {
+      $img.remove()
+    })
+    // cards.splice(randomNum,1)
+    console.log(cards)
+  }
+}
+
+$addDiv.on('click', tarot)
 
 
 
